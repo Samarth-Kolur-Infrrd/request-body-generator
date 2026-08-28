e34c1a1c08986be2d1f1b618660a64e514d72ea3
Samarth-Kolur-Infrrd <samarthdkolur@infrrd.ai>
2026-08-28 15:46:04 +0530

    Refactor MongoDB connection to use environment variable for URI and add example .env file

--- mongoConnection.py
+++ mongoConnection.py
@@ -1,6 +1,12 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
 
 db = client["requestBodyGeneration"]
 
