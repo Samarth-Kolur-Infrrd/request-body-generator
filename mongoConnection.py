c574764370f7b1b5e4ac40bcb54d4b2014505be2
Samarth-Kolur-Infrrd <samarthdkolur@infrrd.ai>
2026-08-31 17:14:04 +0530

    Add error handling and logging to MongoDB data retrieval

--- mongoConnection.py
+++ mongoConnection.py
@@ -1,8 +1,12 @@
 import os
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
 
@@ -15,7 +19,11 @@ pageCollection = db["page"]
 extractionFieldCollection = db["extraction_field"]
 
 def getData(inputId):
-    document = list(documentCollection.find({"_id":inputId["documentId"]}))
-    page = list(pageCollection.find(inputId))
-    field = list(extractionFieldCollection.find(inputId))
-    return [ document, page, field ]
+    try:
+        document = list(documentCollection.find({"_id":inputId["documentId"]}))
+        page = list(pageCollection.find(inputId))
+        field = list(extractionFieldCollection.find(inputId))
+        return [ document, page, field ]
+    except PyMongoError:
+        logger.exception("Mongo query failed for inputId=%s against %s", inputId, MONGODB_URI)
+        raise
